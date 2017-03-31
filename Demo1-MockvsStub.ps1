3cba671e92fd5962facd17e53c1b4ca40c428c86
Chris Hunt <chris.hunt@ticketmaster.com>
2017-03-31 15:18:42 -0400

    Improve Mock example

--- Demo1-MockvsStub.ps1
+++ Demo1-MockvsStub.ps1
@@ -1,22 +1,26 @@
-
 function MockMe ($Path) {
 
     return (Get-ChildItem -Path $Path)
 }
 
 Describe 'Mock' { # Behavior Verification
-
     Mock Get-ChildItem -ParameterFilter {$Path -eq 'C:\foo'} -MockWith {$null}
     Mock Get-ChildItem -ParameterFilter {$Path -eq 'C:\bar'} -MockWith {$null}
 
-    It 'Should call GCI Foo' {
-        $output = MockMe 'C:\foo'
-        Assert-MockCalled Get-ChildItem  -Times 1 -ParameterFilter {$Path -eq 'C:\foo'}
+    Context 'Foo' {
+        It 'Should only call GCI Foo' {
+            $output = MockMe 'C:\foo'
+            Assert-MockCalled Get-ChildItem  -Times 1 -ParameterFilter {$Path -eq 'C:\foo'}
+            Assert-MockCalled Get-ChildItem  -Times 0 -ParameterFilter {$Path -eq 'C:\bar'}
+        }
     }
 
-    It 'Should call GCI Bar' {
-        $output = MockMe 'C:\bar'
-        Assert-MockCalled Get-ChildItem  -Times 1 -ParameterFilter {$Path -eq 'C:\bar'}
+    Context 'Bar' {
+        It 'Should only call GCI Bar' {
+            $output = MockMe 'C:\bar'
+            Assert-MockCalled Get-ChildItem  -Times 0 -ParameterFilter {$Path -eq 'C:\foo'}
+            Assert-MockCalled Get-ChildItem  -Times 1 -ParameterFilter {$Path -eq 'C:\bar'}
+        }
     }
 }
 
