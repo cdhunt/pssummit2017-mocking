8fbeab844d90adab5fa3e4a1f854ce87345b84ad
Chris Hunt <chris.hunt@ticketmaster.com>
2017-04-03 10:09:54 -0400

    Add Demo3

---
+++ Demo3a-Binaries.ps1
@@ -0,0 +1,29 @@
+Describe 'Binaries' {
+
+    Context 'LocalHost' {
+
+        function ping { return @'
+Pinging LOCALHOST [::1] with 32 bytes of data:
+Reply from ::1: time<1ms
+Reply from ::1: time<1ms
+Ping statistics for ::1:
+    Packets: Sent = 2, Received = 2, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 0ms, Maximum = 0ms, Average = 0ms
+'@
+        }
+
+        $results = ping localhost
+        $results | Should Match '0% loss'
+    }
+
+    Context 'Psudo-Behavior Verificaiton' {
+
+        function ping { return $args[0] }
+
+        $results = ping remotehost
+        $results | Should Be 'remotehost'
+
+    }
+}
+
